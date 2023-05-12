b27a89c6029ab2c2d9730c390203c567c24a8172
Albert <albertgiga444@gmail.com>
2023-05-12 15:31:49 +0200

    Optimizations

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-export const FullPizza: React.FC = () => {
+const FullPizza: React.FC = () => {
   const [pizza, setPizza] = React.useState<{
     imageUrl: string;
     title: string;
@@ -35,6 +35,14 @@ export const FullPizza: React.FC = () => {
       <img src={pizza.imageUrl} />
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} ₴</h4>
+      <Link to="/">
+      <button className="button button--outline button--add">
+        <span>Назад</span>
+      </button>
+      </Link>
+      
     </div>
   );
 };
+
+export default FullPizza;
